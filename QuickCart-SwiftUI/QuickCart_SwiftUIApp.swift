881ffd3d44e55cbc4f5ef73e2e0a35204ce30a91
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-03-02 18:35:04 +0300

    Initial Commit

---
+++ QuickCart-SwiftUI/QuickCart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCart_SwiftUIApp.swift
+//  QuickCart-SwiftUI
+//
+//  Created by Dmitry Yastrebov on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuickCart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
